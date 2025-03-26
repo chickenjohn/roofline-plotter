33f2786e630a3fb00b8a9a7ef403f993da08772b
Tianchu.ji <tianchu.ji@cs.stonybrook.edu>
2025-03-26 04:41:52 +0000

    add desc for pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
+# read the contents of README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="roofline-plotter",
-    version="0.1.0",
+    version="0.1.1",
     license="MIT",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
@@ -12,6 +16,8 @@ setup(
     ],
     python_requires=">=3.8",
     description="A simple roofline plotter for visualizing performance data",
+    long_description=long_description,
+    long_description_content_type='text/markdown'
     author="chickenjohn",
     author_email="chickenjohn93@outlook.com",
     url="https://github.com/chickenjohn/roofline-plotter",
